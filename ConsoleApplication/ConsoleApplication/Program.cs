9c7c3cfa17d192c90f566e580e1109bd60bd81c4
tobiasjr <jramnas@gmail.com>
2015-02-17 16:52:27 +0100

    Texts can now be integrated with yooba framework

--- ConsoleApplication/ConsoleApplication/Program.cs
+++ ConsoleApplication/ConsoleApplication/Program.cs
@@ -18,7 +18,7 @@ namespace ConsoleApplication
         {
             var watch = Stopwatch.StartNew();
 
-            string path = @"C:\Users\ex1\desktop\Compare.pptx";
+            string path = @"C:\Users\ex1\desktop\Performance_Out.pptx";
 
             OpenXMLReader reader = new OpenXMLReader(path);
 
