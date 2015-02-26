b2881025bbf79d3e0cdb14d229c2065966e8b2db
tobiasjr <jramnas@gmail.com>
2015-02-26 12:24:33 +0100

    refactor colors and start with backgrounds

--- ConsoleApplication/ConsoleApplication/OpenXMLReader.cs
+++ ConsoleApplication/ConsoleApplication/OpenXMLReader.cs
@@ -62,6 +62,9 @@ namespace ConsoleApplication
                 // Open the presentation file as read-only.
                 using (_presentationDocument = PresentationDocument.Open(_path, false))
                 {
+                    //Read all colors from theme
+                    readTheme();
+
                     //Retrive the presentation part
                     var presentation = _presentationDocument.PresentationPart.Presentation;
 
@@ -179,22 +182,244 @@ namespace ConsoleApplication
         private List<SceneObject> getSceneObjects(PresentationML.Background background)
         {
             List<SceneObject> sceneObjectList = new List<SceneObject>();
+
+            SimpleSceneObject backgroundObject = new SimpleSceneObject();
+
+            ShapeObject shape = new ShapeObject(backgroundObject, ShapeObject.shape_type.Rectangle);
+
+            //FIX ALPHA
+
+            foreach (DrawingML.SolidFill solidFill in background.Descendants<DrawingML.SolidFill>())
+            {
+                shape.FillColor = getColor(solidFill);
+                Console.WriteLine(shape.FillColor);
+
+            }
+            foreach (var gradientFill in background.Descendants<DrawingML.GradientFill>())
+            {
+                List<string> gradientColorList = getColors(gradientFill);
+            }
+                
+
+
+
             return sceneObjectList;
         }
 
-
-        private List<string> getColors(DrawingML.GradientFill gradFill)
+        private List<string> getColors(OpenXmlElement xmlElement)
+        {
+            return getColors(xmlElement, "");
+        }
+        private List<string> getColors(OpenXmlElement xmlElement, string phClr)
         {
             List<string> colors = new List<string>();
 
             return colors;
         }
+        private string getColor(OpenXmlElement xmlElement)
+        {
+            return getColor(xmlElement, "");
+        }
 
-        private string getColor(DrawingML.SolidFill solidFill)
+        private string getColor(OpenXmlElement xmlElement, string phClr)
         {
             string color = "";
 
+            OpenXmlElement colorType = xmlElement.FirstChild;
+            
+            if (colorType.GetType().Equals(typeof(DrawingML.RgbColorModelHex))){
+                
+                foreach (var colorValue in colorType.Parent.Descendants<DrawingML.RgbColorModelHex>())
+                {
+                    if (colorValue.Val.ToString() == "phClr")
+                        color = phClr;
+                    else
+                        color = colorValue.Val.ToString();
+                }
+            }
+            else
+                foreach (var colorValue in colorType.Parent.Descendants<DrawingML.SchemeColor>())
+                    if (colorValue.Val.ToString() == "phClr")
+                        color = phClr;
+                    else
+                        color = getColorFromTheme(colorValue.Val.ToString());
+
+
+            if (colorType.HasChildren)
+            {
+                string transformedColor = colorTransforms(colorType, color);
+                color = transformedColor;
+                return color;
+            }
+            else
+                return color;
+        }
+
+        //Returns a transformed color
+        private string colorTransforms(OpenXmlElement colorInfo, string currentColor)
+        {
+            PowerPointColor powerPointColor = new PowerPointColor();
+
+            powerPointColor.Color = currentColor;
+
+            foreach (var colorAttr in colorInfo.ChildElements)
+            {
+                if (colorAttr.LocalName == "tint")
+                    powerPointColor.Tint = ((DrawingML.Tint)colorAttr).Val;
+                if (colorAttr.LocalName == "shade")
+                    powerPointColor.Shade = ((DrawingML.Shade)colorAttr).Val;
+                if (colorAttr.LocalName == "alpha")
+                    powerPointColor.Alpha = ((DrawingML.Alpha)colorAttr).Val;
+                if (colorAttr.LocalName == "alphaOff")
+                    powerPointColor.AlphaOff = ((DrawingML.AlphaOffset)colorAttr).Val;
+                if (colorAttr.LocalName == "alphaMod")
+                    powerPointColor.AlphaMod = ((DrawingML.AlphaModulation)colorAttr).Val;
+                if (colorAttr.LocalName == "hueMod")
+                    powerPointColor.HueMod = ((DrawingML.HueModulation)colorAttr).Val;
+                if (colorAttr.LocalName == "hueOff")
+                    powerPointColor.HueOff = ((DrawingML.HueOffset)colorAttr).Val;
+                if (colorAttr.LocalName == "sat")
+                    powerPointColor.Sat = ((DrawingML.Saturation)colorAttr).Val;
+                if (colorAttr.LocalName == "satOff")
+                    powerPointColor.SatOff = ((DrawingML.SaturationOffset)colorAttr).Val;
+                if (colorAttr.LocalName == "satMod")
+                    powerPointColor.SatMod = ((DrawingML.SaturationModulation)colorAttr).Val;
+                if (colorAttr.LocalName == "lum")
+                    powerPointColor.Lum = ((DrawingML.Luminance)colorAttr).Val;
+                if (colorAttr.LocalName == "lumOff")
+                    powerPointColor.LumOff = ((DrawingML.LuminanceOffset)colorAttr).Val;
+                if (colorAttr.LocalName == "lumMod")
+                    powerPointColor.Lum = ((DrawingML.LuminanceModulation)colorAttr).Val;
+                if (colorAttr.LocalName == "red")
+                    powerPointColor.Red = ((DrawingML.Red)colorAttr).Val;
+                if (colorAttr.LocalName == "redOff")
+                    powerPointColor.RedOff = ((DrawingML.RedOffset)colorAttr).Val;
+                if (colorAttr.LocalName == "redMod")
+                    powerPointColor.RedMod = ((DrawingML.RedModulation)colorAttr).Val;
+                if (colorAttr.LocalName == "green")
+                    powerPointColor.Green = ((DrawingML.Green)colorAttr).Val;
+                if (colorAttr.LocalName == "greenOff")
+                    powerPointColor.GreenOff = ((DrawingML.GreenOffset)colorAttr).Val;
+                if (colorAttr.LocalName == "greenMod")
+                    powerPointColor.GreenMod = ((DrawingML.GreenModulation)colorAttr).Val;
+                if (colorAttr.LocalName == "blue")
+                    powerPointColor.Blue = ((DrawingML.Blue)colorAttr).Val;
+                if (colorAttr.LocalName == "blueOff")
+                    powerPointColor.BlueOff = ((DrawingML.BlueOffset)colorAttr).Val;
+                if (colorAttr.LocalName == "blueMod")
+                    powerPointColor.BlueMod = ((DrawingML.BlueModulation)colorAttr).Val;
+                //if (colorAttr.LocalName == "gamma")
+                //    powerPointColor.Gamma = ((DrawingML.Gamma)colorAttr).Val;
+                //if (colorAttr.LocalName == "invGamma")
+                //    powerPointColor.InvGamma = ((DrawingML.InverseGamma)colorAttr).Val;
+                //if (colorAttr.LocalName == "comp")
+                //    powerPointColor.Comp = ((DrawingML.Complement)colorAttr).Val;
+                //if (colorAttr.LocalName == "inv")
+                //    powerPointColor.Inv = ((DrawingML.Inverse)colorAttr).Val;
+                //if (colorAttr.LocalName == "gray")
+                //    powerPointColor.Gray = ((DrawingML.Gray)colorAttr).Val;
+
+            }
+            
+            return powerPointColor.getAdjustedColor();
+        }
+
+        private string getColorFromTheme(string color)
+        {
+            for (int i = 0; i < _nrOfThemeColors; i++)
+            {
+                if (color.Equals(_themeColors.GetValue(i, 0).ToString()))
+                {
+                    return (string)_themeColors.GetValue(i, 1);
+                }
+            }
+
             return color;
+        }
+
+        //Read all the colors in the theme
+        private void readTheme()
+        {
+            var slideMaster = _presentationDocument.PresentationPart.SlideMasterParts.ElementAt(0).SlideMaster;
+
+            //Handle the colors
+            var colorScheme = slideMaster.SlideMasterPart.ThemePart.Theme.ThemeElements.ColorScheme;
+            var colorMap = slideMaster.ColorMap;
+
+            int index = 0;
+
+            string[] colorRefNames = new String[_nrOfThemeColors]{ "dk1", "lt1", "dk2", "lt2", 
+                                       "accent1", "accent2", "accent3","accent4","accent5","accent6",
+                                        "hlink", "folHlink", "tx1", "tx2", "bg1", "bg2"};
+
+            foreach (var childElement in colorScheme.ChildElements)
+            {
+                string color = "";
+
+                foreach (DrawingML.RgbColorModelHex rgbColor in childElement.Descendants<DrawingML.RgbColorModelHex>())
+                {
+                    color = rgbColor.Val;
+                }
+
+                foreach (DrawingML.SystemColor systemColor in childElement.Descendants<DrawingML.SystemColor>())
+                {
+                    color = systemColor.LastColor;
+                }
+
+                _themeColors[index, 0] = colorRefNames[index];
+                _themeColors[index, 1] = color;
+
+                index++;
+            }
+
+            if (colorExistInTheme(colorMap.Text1.InnerText))
+            {
+                _themeColors[index, 0] = colorRefNames[index];
+                _themeColors[index, 1] = getColorFromTheme(colorMap.Text1.InnerText);
+            }
+            index++;
+            if (colorExistInTheme(colorMap.Text2.InnerText))
+            {
+                _themeColors[index, 0] = colorRefNames[index];
+                _themeColors[index, 1] = getColorFromTheme(colorMap.Text2.InnerText);
+            }
+            index++;
+            if (colorExistInTheme(colorMap.Background1.InnerText))
+            {
+                _themeColors[index, 0] = colorRefNames[index];
+                _themeColors[index, 1] = getColorFromTheme(colorMap.Background1.InnerText);
+            }
+            index++;
+            if (colorExistInTheme(colorMap.Background2.InnerText))
+            {
+                _themeColors[index, 0] = colorRefNames[index];
+                _themeColors[index, 1] = getColorFromTheme(colorMap.Background2.InnerText);
+            }
+
+            //Handle the line attr
+            var formatScheme = slideMaster.SlideMasterPart.ThemePart.Theme.ThemeElements.FormatScheme;
+
+            if (formatScheme.LineStyleList.HasChildren)
+            {
+                foreach (DrawingML.Outline ln in formatScheme.LineStyleList)
+                {
+                    _themeLines.Add(ln);
+                }
+            }
+
+
+        }
+
+        public bool colorExistInTheme(string color)
+        {
+            for (int i = 0; i < _nrOfThemeColors; i++)
+            {
+                if (color == _themeColors.GetValue(i, 0).ToString())
+                {
+                    return true;
+                }
+            }
+            return false;
         }
 
     }
